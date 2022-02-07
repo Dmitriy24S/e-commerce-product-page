6f36a327db8a676f1f7f46a48413bc3f5bc7e88f
Dmitriy <devilins24@gmail.com>
2022-02-07 19:14:11 +0200

    add fullscreen image toggle/overlay view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import HamburgerMenuIcon from "./images/icon-menu.svg";
 import HamburgerMenuCloseIcon from "./images/icon-close.svg";
+import { ReactComponent as CloseIcon } from "./images/icon-close.svg";
 import CompanyLogo from "./images/logo.svg";
 import CartIcon from "./images/icon-cart.svg";
 import CustomerIcon from "./images/image-avatar.png";
@@ -9,12 +10,14 @@ import LeftArrow from "./images/icon-previous.svg";
 import RightArrow from "./images/icon-next.svg";
 import PlusIcon from "./images/icon-plus.svg";
 import MinusIcon from "./images/icon-minus.svg";
+import TrashIcon from "./images/icon-delete.svg";
 import { ProductData } from "./SliderImagesData";
 
 function App() {
   const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [cartAmount, setCartAmount] = useState(0);
+  const [isImageFullscreen, setIsImageFullscreen] = useState(false);
 
   const prevSlide = () => {
     setCurrentIndex(currentIndex === 0 ? ProductData[0].images.length - 1 : currentIndex - 1);
@@ -58,27 +61,32 @@ function App() {
         </div>
         {/* Right Header */}
         <div className="header-right flex items-center gap-4 p-2 pt-4 pb-5 sm:p-10 sm:pt-4 sm:pb-5">
-          <img src={CartIcon} alt="cart" />
-          <img src={CustomerIcon} alt="customer" className="w-6" />
+          <img src={CartIcon} className="cursor-pointer" alt="cart" />
+          <img src={CustomerIcon} alt="customer" className="w-6 cursor-pointer" />
         </div>
       </header>
 
       {/* Main */}
-      <main>
+      <main className="relative">
         {/* image container */}
         <div className="image-container relative flex flex-col gap-4 overflow-hidden">
-          {ProductData[0].images.map((image, index) => {
-            return (
-              <img
-                src={process.env.PUBLIC_URL + image.image}
-                alt="sneaker"
-                className={currentIndex === index ? "block product-image" : "hidden"}
-                key={index}
-              />
-            );
-          })}
-          <img src={LeftArrow} alt="left arrow" className="left-arrow" onClick={prevSlide} />
-          <img src={RightArrow} alt="right arrow" className="right-arrow" onClick={nextSlide} />
+          <div className="arrow-wrapper">
+            {ProductData[0].images.map((image, index) => {
+              return (
+                <img
+                  src={process.env.PUBLIC_URL + image.image}
+                  alt="sneaker"
+                  className={
+                    currentIndex === index ? "block product-image md:cursor-pointer" : "hidden"
+                  }
+                  key={index}
+                  onClick={() => setIsImageFullscreen(!isImageFullscreen)}
+                />
+              );
+            })}
+            <img src={LeftArrow} alt="left arrow" className="left-arrow" onClick={prevSlide} />
+            <img src={RightArrow} alt="right arrow" className="right-arrow" onClick={nextSlide} />
+          </div>
 
           {/* Thumbnails */}
           <div className="image-previews hidden md:flex md:gap-3 justify-center mb-4 mt-2">
@@ -119,7 +127,7 @@ function App() {
           <div className="product-description">
             <p>{ProductData[0].description}</p>
           </div>
-          {/* start price container */}
+          {/* price container */}
           <div className="price-container flex justify-between">
             {ProductData[0].discount ? (
               <>
@@ -140,7 +148,7 @@ function App() {
             )}
           </div>
           {/* end price container */}
-          {/* start button container  */}
+          {/* button container  */}
           <div className="btn-container flex flex-col gap-4 md:flex-row">
             <div className="amount-container flex justify-between py-3 px-0 rounded-lg mt-2 gap-2 md:mt-0 md:min-w-fit">
               <button
@@ -162,6 +170,68 @@ function App() {
           {/* end button container */}
         </div>
         {/* end product info */}
+
+        {/* Fullscreen image overlay */}
+        {isImageFullscreen && (
+          <div className="fullscreen-image-overlay">
+            <div className="wrapper">
+              <CloseIcon
+                className="fullscreen-img-close-btn2"
+                fill="white"
+                stroke="orange"
+                onClick={() => setIsImageFullscreen(!isImageFullscreen)}
+              />
+              {/* fullscreen image container */}
+              <div className="image-container relative flex flex-col gap-4 overflow-hidden">
+                {ProductData[0].images.map((image, index) => {
+                  return (
+                    <img
+                      src={process.env.PUBLIC_URL + image.image}
+                      alt="sneaker"
+                      className={currentIndex === index ? "block product-image" : "hidden"}
+                      key={index}
+                    />
+                  );
+                })}
+                <img src={LeftArrow} alt="left arrow" className="left-arrow" onClick={prevSlide} />
+                <img
+                  src={RightArrow}
+                  alt="right arrow"
+                  className="right-arrow"
+                  onClick={nextSlide}
+                />
+              </div>
+            </div>
+            {/* fullscreen thumbnails */}
+            <div className="image-previews hidden md:flex md:gap-3 justify-center mb-4 mt-2">
+              {ProductData[0].images.map((image, index) => {
+                return (
+                  <div
+                    className={
+                      currentIndex === index
+                        ? "product-thumbnail active-thumbnail cursor-pointer"
+                        : "product-thumbnail cursor-pointer"
+                    }
+                    onClick={() => setCurrentIndex(index)}
+                  >
+                    <img
+                      src={process.env.PUBLIC_URL + image.thumbnail}
+                      alt="sneaker preview"
+                      className={
+                        currentIndex === index
+                          ? "product-thumbnail active-thumbnail"
+                          : "product-thumbnail"
+                      }
+                      key={index}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            {/* end fullscreen image container */}
+          </div>
+        )}
+        {/* end fullscreen image overlay */}
       </main>
     </div>
   );
